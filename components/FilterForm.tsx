c1dcc4408743f25e1787a794b55e30a29608ead7
Tamas Lorincz <tamas.lorincz@solarwinds.com>
2025-04-14 11:59:47 +0200

    Fix Applicaiton error

--- components/FilterForm.tsx
+++ components/FilterForm.tsx
@@ -31,7 +31,7 @@ interface FilterFormProps {
   control: any;
   register: any;
   handleSubmit: (
-    fn: (data: FilterFormValues) => void,
+    fn: (data: FilterFormValues) => void
   ) => (e?: React.BaseSyntheticEvent) => Promise<void>;
   ghToken: string;
 }
@@ -56,7 +56,7 @@ export const FilterForm: React.FC<FilterFormProps> = ({
   const { data, isLoading, mutate, isValidating } = useSWR<Data>(
     ghToken ? "/api/my-repos" : null,
     (url: string) => fetcher(url, ghToken || ""),
-    { revalidateOnFocus: false, errorRetryCount: 0 },
+    { revalidateOnFocus: false, errorRetryCount: 0 }
   );
 
   useEffect(() => {
@@ -84,7 +84,7 @@ export const FilterForm: React.FC<FilterFormProps> = ({
             selectedKeys={[field.value]}
             onChange={(event) => field.onChange(event.target.value)}
           >
-            {data
+            {data?.repos
               ? Object.entries(data.repos).map(([groupName, repos]) => (
                   <SelectSection key={groupName} showDivider title={groupName}>
                     {repos?.map((repo) => (
